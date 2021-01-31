a395a186e6a33da32064566d37b1656620a70e2c
Ji Hun Keom <63485440+jihun0902@users.noreply.github.com>
2021-01-31 09:01:38 +0900

    Delete a.py

--- a.py
+++
@@ -1,6 +0,0 @@
-import re
-import string
-
-corpus = open('../data/news1.txt').read()
-corpus = [(re.sub(r'\d+', '', corpus.lower()).translate(str.maketrans('','', string.punctuation))).strip().split()]
-
